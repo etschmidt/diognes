e41850ca2f4774ac86d8e71707e7471d88e0a1d8
Ethan <ethan.t.schmidt@gmail.com>
2017-06-20 23:47:28 -0700

    asdf

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
-  root to: "static_pages#about"
+  root "static_pages#about" #this needs to be changed
 
   get '/about', to: "static_pages#about"
 
   devise_for :users, path: 'u'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, :path => 'u'
