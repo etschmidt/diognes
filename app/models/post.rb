c4941e20fc6c7419752b8217c14ba9b23edc2ffd
Ethan <ethan.t.schmidt@gmail.com>
2017-06-28 14:05:49 -0700

    post characater max

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
-  validates :content, presence: true, length: { maximum: 1701 }
+  validates :content, presence: true, length: { maximum: 50000 }
 end
