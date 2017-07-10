f719500e6c6d6e7ee29237920f3862e4cb5da7b8
Ethan <ethan.t.schmidt@gmail.com>
2017-07-09 23:36:28 -0700

    asdf

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,10 @@ class RegistrationsController < Devise::RegistrationsController
 
   private
 
+  def after_update_path_for(resource)
+    user_path(resource)
+  end
+    
   def sign_up_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :description, :private)
   end
